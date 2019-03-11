8d821e902862083da0d6da0540e58ec58e47b030
Multikai <43823556+Multikai@users.noreply.github.com>
2019-03-11 08:42:23 -0700

    Implementing planet obj, player obj created

--- src/Planet.java
+++ src/Planet.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class Planet {
 
-    private static final int MAX_SIZE = 300;
+    private static final int MAX_SIZE = 250;
     private static final int MIN_SIZE = 100;
     private static final int MEAN_SIZE = (MAX_SIZE + MIN_SIZE) / 2;
     private static final double STDV = 100;
